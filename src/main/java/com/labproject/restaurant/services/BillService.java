20ac61aa7ddcdf04f14e96897230c297660fdebb
Zhanna Fedorova <Zhanna_Fedorova@epam.com>
2018-10-17 12:22:53 +0300

    Added BillService

---
+++ src/main/java/com/labproject/restaurant/services/BillService.java
@@ -0,0 +1,13 @@
+package com.labproject.restaurant.services;
+
+import com.labproject.restaurant.entities.Bill;
+
+public interface BillService {
+    Bill getById(long id);
+
+    void insert(Bill role);
+
+    void update(Bill role);
+
+    void delete(Bill role);
+}
