e03d5606bacf48380e0eae91d333ee50d7b6d98d
Zhanna Fedorova <Zhanna_Fedorova@epam.com>
2018-10-20 14:20:55 +0300

    Added some javadoc comments

--- src/main/java/com/labproject/restaurant/dao/BillDao.java
+++ src/main/java/com/labproject/restaurant/dao/BillDao.java
@@ -3,16 +3,53 @@ package com.labproject.restaurant.dao;
 import com.labproject.restaurant.entities.Bill;
 
 import java.util.List;
-import java.util.Set;
 
+/**
+ * This is a DAO interface for Bill entity
+ */
 public interface BillDao {
+
+    /**
+     * Returns a Bill object from Bill database table
+     * by the specified id
+     *
+     * @param id a primary key of Bill table
+     * @return the Bill object at the specified id
+     * @see Bill
+     */
     Bill getById(long id);
 
+    /**
+     * Returns a List of Bill objects from Bill database table
+     *
+     * @return the list of Bill objects
+     * @see Bill
+     */
     List<Bill> getAll();
 
+    /**
+     * Inserts the line in Bill database table.
+     *
+     * @param bill the Bill entity
+     * @see Bill
+     */
     void insert(Bill bill);
 
+    /**
+     * Changes the line in Bill database table with id specified
+     * in the parameter bill
+     *
+     * @param bill the Bill entity
+     * @see Bill
+     */
     void update(Bill bill);
 
-    void deleteById(Long id);
+    /**
+     * Deletes a line from "Bill" database table
+     * by a specified id
+     *
+     * @param id a primary key of Bill table
+     * @see Bill
+     */
+    void deleteById(long id);
 }
--- src/main/java/com/labproject/restaurant/dao/impl/BillDaoImpl.java
+++ src/main/java/com/labproject/restaurant/dao/impl/BillDaoImpl.java
@@ -4,9 +4,7 @@ import com.labproject.restaurant.dao.BillDao;
 import com.labproject.restaurant.entities.Bill;
 import com.labproject.restaurant.entities.Order;
 import com.labproject.restaurant.entities.User;
-import org.apache.commons.dbutils.DbUtils;
 import org.apache.log4j.Logger;
-import org.springframework.util.Assert;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -16,11 +14,15 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * This is a DAO class for Bill entity that implements the
+ * {@code BillDao} interface
+ */
 public class BillDaoImpl implements BillDao {
 
+    private static final Logger LOGGER = Logger.getLogger(UserDaoImpl.class);
     private DataSource dataSource;
-
-    private final Logger logger = Logger.getLogger(UserDaoImpl.class);
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
@@ -28,40 +30,40 @@ public class BillDaoImpl implements BillDao {
 
     @Override
     public Bill getById(long id) {
-        String request = "SELECT ID, ORDERID, ADMINID, AMOUNT, `DATE`, " +
+        String query = "SELECT ID, ORDERID, ADMINID, AMOUNT, `DATE`, " +
                 "`STATUS` FROM bill where id=?;";
-        Bill result = new Bill();
+        Bill bill = new Bill();
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(request)) {
+             PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setLong(1, id);
             ResultSet rs = ps.executeQuery();
             if (rs.next()) {
-                result.setId(rs.getLong("id"));
+                bill.setId(rs.getLong("id"));
                 Order order = new Order();
                 order.setId(rs.getLong("OrderId"));
-                result.setOrder(order);
+                bill.setOrder(order);
                 User admin = new User();
                 admin.setId(rs.getLong("AdminId"));
-                result.setAdmin(admin);
-                result.setAmount(rs.getBigDecimal("Amount"));
-                result.setDate(rs.getTimestamp("Date"));
-                result.setStatus(rs.getBoolean("Status"));
+                bill.setAdmin(admin);
+                bill.setAmount(rs.getBigDecimal("Amount"));
+                bill.setDate(rs.getTimestamp("Date"));
+                bill.setStatus(rs.getBoolean("Status"));
             }
         } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
         }
-        return result;
+        return bill;
     }
 
     @Override
     public List<Bill> getAll() {
-        String request = "SELECT ID, ORDERID, ADMINID, AMOUNT, `DATE`, " +
+        String query = "SELECT ID, ORDERID, ADMINID, AMOUNT, `DATE`, " +
                 "`STATUS` FROM bill;";
 
-        List<Bill> result = new ArrayList<>();
+        List<Bill> bills = new ArrayList<>();
 
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(request)) {
+             PreparedStatement ps = connection.prepareStatement(query)) {
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
                 Bill bill = new Bill();
@@ -75,20 +77,20 @@ public class BillDaoImpl implements BillDao {
                 bill.setAmount(rs.getBigDecimal("Amount"));
                 bill.setDate(rs.getTimestamp("Date"));
                 bill.setStatus(rs.getBoolean("Status"));
-                result.add(bill);
+                bills.add(bill);
             }
         } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
         }
-        return result;
+        return bills;
     }
 
     @Override
     public void insert(Bill bill) {
-        String request = "INSERT INTO Bill (ID, ORDERID, ADMINID, AMOUNT, `DATE`, `STATUS`) " +
-                "VALUES (?, ?, ?, ?, ?, ?)";
+        String query = "INSERT INTO Bill (ID, ORDERID, ADMINID, AMOUNT, `DATE`, `STATUS`) " +
+                "VALUES (?, ?, ?, ?, ?, ?);";
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(request)) {
+             PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setLong(1, bill.getId());
             ps.setLong(2, bill.getOrder().getId());
             ps.setLong(3, bill.getAdmin().getId());
@@ -96,17 +98,17 @@ public class BillDaoImpl implements BillDao {
             ps.setTimestamp(5, bill.getDate());
             ps.setBoolean(6, bill.getStatus());
         } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
     @Override
     public void update(Bill bill) {
-        String request = "UPDATE Bill SET `ORDERID`=?, `ADMINID`=?, `AMOUNT`=?, `DATE`=?," +
+        String query = "UPDATE Bill SET `ORDERID`=?, `ADMINID`=?, `AMOUNT`=?, `DATE`=?," +
                 " `STATUS`=? WHERE id=?;";
 
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(request)) {
+             PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setLong(1, bill.getOrder().getId());
             ps.setLong(2, bill.getAdmin().getId());
             ps.setBigDecimal(3, bill.getAmount());
@@ -115,19 +117,19 @@ public class BillDaoImpl implements BillDao {
             ps.setLong(6, bill.getId());
             ps.executeUpdate();
         } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
         }
     }
 
     @Override
-    public void deleteById(Long id) {
-        String request = "DELETE FROM `BILL` WHERE ID = ?;";
+    public void deleteById(long id) {
+        String query = "DELETE FROM `BILL` WHERE ID = ?;";
 
         try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement(request)) {
+             PreparedStatement ps = connection.prepareStatement(query)) {
             ps.setLong(1, id);
         } catch (SQLException e) {
-            logger.error(e.getMessage(), e);
+            LOGGER.error(e.getMessage(), e);
         }
     }
 }
