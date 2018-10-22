d9cf4debc37880c455980ee748b3c6ffc62c65de
Aleksandr Zorin <Aleksandr_Zorin@epam.com>
2018-10-22 22:40:13 +0300

    Migrate to ServletAPI 3.1

--- src/main/java/com/labproject/restaurant/controllers/MenuController.java
+++ src/main/java/com/labproject/restaurant/controllers/MenuController.java
@@ -1,11 +1,9 @@
 package com.labproject.restaurant.controllers;
 
 
-import com.labproject.restaurant.entities.DishEntity;
 import com.labproject.restaurant.services.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,37 +15,37 @@ import java.util.Map;
 
 @Controller
 public class MenuController {
-        @Autowired
-        private DishService dishService;
+    @Autowired
+    private DishService dishService;
 
     public void setDishService(DishService dishService) {
         this.dishService = dishService;
     }
 
-    @RequestMapping(value = "/dishes",method = RequestMethod.GET)
+    @RequestMapping(value = "/dishes", method = RequestMethod.GET)
     public ModelAndView showDishes(HttpServletRequest req, HttpServletResponse resp) {
         ModelAndView mav = new ModelAndView("dishes");
         mav.addObject("dishesList", dishService.getAll());
         return mav;
     }
 
-    @RequestMapping(value = "/dishes",method = RequestMethod.POST)
-    public void addToCart(HttpServletRequest req, HttpServletResponse resp,  @ModelAttribute("dish") DishEntity dish) {
-        System.out.println(dish.getId());
+    @RequestMapping(value = "/dishes", method = RequestMethod.POST)
+    public void addToCart(HttpServletRequest req, HttpServletResponse resp) {
         Object objCartMap = req.getSession().getAttribute("cartMap");
         int count = Integer.parseInt(req.getParameter("count"));
-        Map <DishEntity, Integer> cartMap;
+        long dishId = Long.parseLong(req.getParameter("id"));
+        Map<Long, Integer> cartMap;
         if (objCartMap == null) {
             cartMap = new HashMap<>();
         } else {
-            cartMap = (Map<DishEntity, Integer>)objCartMap;
+            cartMap = (Map<Long, Integer>) objCartMap;
         }
-        Integer amounnt = cartMap.get(dish);
-        if (amounnt == null) {
-            amounnt = 0;
+        Integer amount = cartMap.get(dishId);
+        if (amount == null) {
+            amount = 0;
         }
-        cartMap.put(dish,amounnt+count);
-        System.out.println(cartMap.get(dish));
+        cartMap.put(dishId, amount + count);
+        System.out.println(dishId + " " + cartMap.get(dishId));
         req.getSession().setAttribute("cartMap", cartMap);
         resp.setStatus(HttpServletResponse.SC_ACCEPTED);
     }
