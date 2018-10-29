1a20f0c48ecefcce361352aaa91c26931a19065a
Aleksandr Zorin <Aleksandr_Zorin@epam.com>
2018-10-29 11:03:27 +0300

    add bootstrap style to login,register,dish. add navigate.jsp

--- src/main/webapp/resources/js/cart.js
+++ src/main/webapp/resources/js/cart.js
@@ -10,3 +10,14 @@ function deleteFromCart(dishId) {
         success: window.location.replace(window.location)
     });
 }
+
+function addToCart(dishId) {
+    $.ajax({
+        url: "cart",
+        type: "POST",
+        data: {
+            id: dishId,
+            count: $('[name = pieces_' + dishId + ']').val()
+        }
+    });
+}
