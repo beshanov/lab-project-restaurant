2161afe06799011eeb7ecb68b680f4f68ac2dc10
Aleksandr Zorin <Aleksandr_Zorin@epam.com>
2018-10-31 19:07:07 +0300

    add logger message while exception occurred

--- src/main/java/com/labproject/restaurant/dao/mapping/UserMapper.java
+++ src/main/java/com/labproject/restaurant/dao/mapping/UserMapper.java
@@ -2,12 +2,16 @@ package com.labproject.restaurant.dao.mapping;
 
 import com.labproject.restaurant.entities.Role;
 import com.labproject.restaurant.entities.User;
+import org.apache.log4j.Logger;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserMapper implements RowMapper<User> {
+
+    private static final Logger LOGGER = Logger.getLogger(UserMapper.class);
+
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
         User user = new User();
@@ -21,6 +25,7 @@ public class UserMapper implements RowMapper<User> {
         try {
             role.setName(rs.getString("NAME"));
         } catch (SQLException e) {
+            LOGGER.info("Lazy load, haven't column NAME");
         }
         user.setRole(role);
         return user;
